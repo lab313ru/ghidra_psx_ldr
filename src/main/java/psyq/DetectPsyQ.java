3665e06b03910fa39c06437164d9d99704e6acca
Vladimir Kononovich <meffi@lab313.ru>
2020-01-04 03:18:31 +0300

    Fixed PsyQ version detect.

--- src/main/java/psyq/DetectPsyQ.java
+++ src/main/java/psyq/DetectPsyQ.java
@@ -7,8 +7,8 @@ import ghidra.program.model.mem.MemoryAccessException;
 import ghidra.util.task.TaskMonitor;
 
 public class DetectPsyQ {
-	private final static byte[] VERSION_BYTES = new byte[] {0x50, 0x73, 0x07, 0x00, 0x00, 0x00, 0x47, 0x00}; // 0x47 - a version
-	private final static byte[] VERSION_MASK = new byte[] {(byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF, 0x00, (byte)0xFF};
+	private final static byte[] VERSION_BYTES = new byte[] {     0x50,       0x73,       0x07, 0x00, 0x00, 0x00, 0x47,       0x00}; // 0x47 - a version
+	private final static byte[] VERSION_MASK = new byte[] {(byte)0xFF, (byte)0xFF, (byte)0xFF, 0x00, 0x00, 0x00, 0x00, (byte)0xFF};
 	private final static long VERSION_OFFSET = 0x06L;
 	
 	public static String getPsyqVersion(Memory mem, Address startAddress) throws MemoryAccessException, AddressOutOfBoundsException {
