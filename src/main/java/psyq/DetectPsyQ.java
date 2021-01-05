72c83c9aae75663448b3e3cdc9bdf810d3e4668b
Nick Fox <nick@foxsec.net>
2021-01-05 16:33:20 -0500

    fix whitespace

--- src/main/java/psyq/DetectPsyQ.java
+++ src/main/java/psyq/DetectPsyQ.java
@@ -13,7 +13,7 @@ public class DetectPsyQ {
 	private final static String[] PSYQ_SIG_VERSIONS = new String[] {
 			"3.6", "4.0", "4.1",
 			"4.2", "4.3", "4.4",
-	        "4.5", "4.6", "4.7"
+			"4.5", "4.6", "4.7"
 	};
 	
 	public static String getPsyqVersion(Memory mem, Address startAddress) throws MemoryAccessException, AddressOutOfBoundsException {
