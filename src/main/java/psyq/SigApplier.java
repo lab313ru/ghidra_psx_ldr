69f9197a342f9339a97d5e7fa7b837a2da57a7b0
Vladimir Kononovich <newinferno@gmail.com>
2021-01-12 01:11:08 +0300

    Added zero at the end of versions.

--- src/main/java/psyq/SigApplier.java
+++ src/main/java/psyq/SigApplier.java
@@ -8,10 +8,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.io.File;
 
 import com.google.gson.JsonArray;
